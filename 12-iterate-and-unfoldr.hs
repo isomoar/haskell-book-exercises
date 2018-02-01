3c3711d877507a8720e22118e7195d446aba889b
Jane G <isomoar@gmail.com>
2018-02-01 11:56:31 +0300

    Done with iterate and unfoldr

--- 12-iterate-and-unfoldr.hs
+++ 12-iterate-and-unfoldr.hs
@@ -2,5 +2,10 @@ myIterate :: (a -> a) -> a -> [a]
 myIterate f n = n : myIterate f (f n)
 
 
--- myUnfoldr :: (b -> Maybe (a, b)) -> b -> [a]
--- myUnfoldr f n = n : (myUnfoldr n)
+myUnfoldr :: (b -> Maybe (a, b)) -> b -> [a]
+myUnfoldr f x = case f x of
+                  Just (a, b) -> a : myUnfoldr f b
+                  Nothing     -> []
+
+beterIterate :: (a -> a) -> a -> [a]
+beterIterate f x = myUnfoldr (\a -> Just (a, f a)) x
