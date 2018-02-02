bce0206142942760b89b32c10143bdc531024c74
Jane G <isomoar@gmail.com>
2018-02-02 17:53:13 +0300

    Add property test for exersices

--- 14-testing/exercises/tests/tests.hs
+++ 14-testing/exercises/tests/tests.hs
@@ -1,8 +1,59 @@
 module WordNumberTest where
 
+import           Data.List       (sort)
 import           Test.Hspec
-import           WordNumber (digitToWord, digits, wordNumber)
+import           Test.QuickCheck
+import           WordNumber      (digitToWord, digits, wordNumber)
 
+
+-- test properties
+--
+half x = x / 2
+
+halfIdentity = (*2) . half
+
+prop_lessThanAbs :: Double -> Bool
+prop_lessThanAbs x = (half x) > (abs x)
+
+prop_next :: Double -> Bool
+prop_next x = (halfIdentity x) == x
+
+listOrdered :: (Ord a) => [a] -> Bool
+listOrdered xs =
+  snd $ foldr go (Nothing, True) xs
+    where go _ status@(_, False) = status
+          go y (Nothing, t)      = (Just y, t)
+          go y (Just x, t)       = (Just y, x >= y)
+
+plusAssociative x y z =
+  x + (y + z) == (x + y) + z
+
+prop_plusAssociative :: Int -> Int -> Int -> Bool
+prop_plusAssociative  = plusAssociative
+
+plusCommutative x y =
+  x + y == y + x
+
+prop_plusCommutative :: Int -> Int -> Bool
+prop_plusCommutative  = plusCommutative
+
+prop_listOrdered :: [Int] -> Bool
+prop_listOrdered list = if listOrdered list then True else False
+
+testProperties :: IO ()
+testProperties = hspec $ do
+  it "x / 2 is always less than abs x" $ do
+    quickCheck prop_lessThanAbs
+  it "(*2) . half is always == x" $ do
+    quickCheck prop_next
+  it "should be ordered list" $ do
+    quickCheck prop_listOrdered
+  it "x + (y + z) == (x + y) + z" $ do
+    quickCheck prop_plusAssociative
+  it "x + y == y + x" $ do
+    quickCheck prop_plusCommutative
+
+-- test WordNumber module
 
 main :: IO ()
 main = hspec $ do
