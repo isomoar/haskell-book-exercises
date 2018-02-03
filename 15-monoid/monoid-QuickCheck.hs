a2a684b9f858f5bc96779f10fc840a2a9919c30b
Jane G <isomoar@gmail.com>
2018-02-04 00:51:01 +0300

    First' monoid

--- 15-monoid/monoid-QuickCheck.hs
+++ 15-monoid/monoid-QuickCheck.hs
@@ -15,20 +15,65 @@ monoidRightIdentity :: (Eq m, Monoid m) => m -> Bool
 monoidRightIdentity a = (a <> mempty) == a
 
 
-data Bull = Fools | Twoo deriving (Eq, Show)
+-- data Bull = Fools | Twoo deriving (Eq, Show)
+--
+-- instance Arbitrary Bull where
+--   arbitrary = frequency [ (1, return Fools)
+--                         , (1, return Twoo)]
+--
+-- instance Monoid Bull where
+--   mempty = Fools
+--   mappend _ _ = Fools
+--
+-- type BullMappend = Bull -> Bull -> Bull -> Bool
 
-instance Arbitrary Bull where
-  arbitrary = frequency [ (1, return Fools)
-                        , (1, return Twoo)]
+-- testInvalidMonoid :: IO ()
+-- testInvalidMonoid = do
+--   quickCheck (monoidAssoc :: BullMappend)
+--   quickCheck (monoidLeftIdentity :: Bull -> Bool)
+--   quickCheck (monoidRightIdentity :: Bull -> Bool)
 
-instance Monoid Bull where
-  mempty = Fools
-  mappend _ _ = Fools
+data Optional a = Nada | Only a deriving (Eq, Show)
 
-type BullMappend = Bull -> Bull -> Bull -> Bool
+instance Monoid a => Monoid (Optional a) where
+  mempty = Nada
+  mappend Nada x            = x
+  mappend x Nada            = x
+  mappend (Only x) (Only y) = Only (mappend x y)
 
-testInvalidMonoid :: IO ()
-testInvalidMonoid = do
-  quickCheck (monoidAssoc :: BullMappend)
-  quickCheck (monoidLeftIdentity :: Bull -> Bool)
-  quickCheck (monoidRightIdentity :: Bull -> Bool)
+
+newtype First' a = First' { getFirst' :: Optional a } deriving (Eq, Show)
+
+instance  Monoid (First' a) where
+  mempty = First' Nada
+  mappend (First' Nada) (First' Nada) = First' Nada
+  mappend (First' Nada) x             = x
+  mappend x _                         = x
+
+optionalGen :: Arbitrary a => Gen (Optional a)
+optionalGen = do
+  a <- arbitrary
+  frequency [ (1, return Nada )
+            , (1, return (Only a))
+            ]
+
+instance (Arbitrary a) => Arbitrary (Optional a) where
+  arbitrary = optionalGen
+
+firstGen :: Arbitrary a => Gen (First' a)
+firstGen = do
+  a <- arbitrary
+  frequency [ (1, return (First' Nada) )
+            , (1, return (First' a))
+            ]
+
+instance (Arbitrary a) => Arbitrary (First' a) where
+  arbitrary = firstGen
+
+type FirstAssoc = First' Int -> First' Int -> First' Int -> Bool
+
+testFirst :: IO ()
+testFirst = do
+  quickCheck (monoidAssoc :: FirstAssoc)
+  quickCheck (monoidLeftIdentity :: First' Int -> Bool)
+  quickCheck (monoidRightIdentity :: First' Int -> Bool)
