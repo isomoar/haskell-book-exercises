64cb3aca2e339243d90cf4c5a0020fb3cb64f36f
Jane G <isomoar@gmail.com>
2018-02-12 12:32:15 +0300

    Fold exercises

--- 20-foldable/exercises.hs
+++ 20-foldable/exercises.hs
@@ -1,8 +1,35 @@
-data Constant a b = Constant a
+import           Data.Monoid
 
-data Two a b = Two a b
+data Constant a b = Constant a deriving ( Show)
+
+data Two a b = Two a b deriving (Show)
 
 data Three a b c = Three a b c
 
+data Three' a b = Three' a b b
+
+data Four' a b = Four' a b b b
+
 instance Foldable (Constant a) where
-  foldr f z (Constant x) = f x z
+  foldMap _ _ = mempty
+
+instance Foldable (Two a) where
+  foldMap f (Two a b) = f b
+  foldr f z (Two a b) = f b z
+
+instance Foldable (Three a b) where
+  foldMap f (Three a b c) = f c
+  foldr f z (Three a b c) = f c z
+
+instance Foldable (Three' a) where
+  foldMap f (Three' a b c) = f b <> f c
+
+instance Foldable (Four' a) where
+  foldMap f (Four' a b c d) = f b <> f c <> f d
+
+
+filterF :: (Applicative f, Foldable t, Monoid (f a))
+        => (a -> Bool) -> t a -> f a
+filterF f = foldMap (\x -> if f x then (pure x) else mempty)
+
+
