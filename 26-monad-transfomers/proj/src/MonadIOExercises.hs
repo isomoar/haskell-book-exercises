a5b70f64874e8b2e471ea10bb36d00f451e2b40f
Jane G <isomoar@gmail.com>
2018-02-26 18:35:33 +0300

    MonadIO

---
+++ 26-monad-transfomers/proj/src/MonadIOExercises.hs
@@ -0,0 +1,25 @@
+import           Control.Applicative       (liftA2)
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Class
+-- import           Control.Monad.Trans.Identity
+
+newtype MaybeT m a =
+  MaybeT { runMaybeT :: m (Maybe a) }
+
+instance Functor m => Functor (MaybeT m) where
+  fmap f (MaybeT mma) = MaybeT (fmap (fmap f) mma)
+
+instance Applicative m => Applicative (MaybeT m) where
+  pure = MaybeT . pure . pure
+
+  MaybeT mmab <*> MaybeT mma =
+    MaybeT $ liftA2 (<*>) mmab mma
+
+instance Monad m => Monad (MaybeT m) where
+  return = pure
+
+  MaybeT mma >>= f =
+    MaybeT $ mma >>= maybe (pure Nothing) (runMaybeT . f)
+
+instance (MonadIO m) => MonadIO (MaybeT m) where
+  liftIO = undefined
--- 26-monad-transfomers/proj/src/scotty.hs
+++ 26-monad-transfomers/proj/src/scotty.hs
@@ -2,35 +2,12 @@
 
 module Scotty where
 
-import           Control.Monad
-import           Control.Monad.Trans.Class
-import           Control.Monad.Trans.Except
-import           Control.Monad.Trans.Reader
-import           Control.Monad.Trans.State.Lazy hiding (get)
-import           Data.Monoid                    (mconcat)
+import           Control.Monad.IO.Class
+import           Data.Monoid            (mconcat)
 import           Web.Scotty
-import           Web.Scotty.Internal.Types      (ActionT (..))
-
--- newtype ActionT e m a =
---   ActionT {
---     runAM :: ExceptT (ActionError e)
---                      (ReaderT ActionEnv
---                           (StateT ScottyResponse m)) a }
---       deriving (Functor, Applicative)
-
-liftReaderT :: m a -> ReaderT r m a
-liftReaderT m = ReaderT (const m)
 
 main = scotty 3000 $ do
   get "/:word" $ do
     beam <- param "word"
-    -- (lift :: IO a -> ActionM a) (putStrLn "hello")
-    (ActionT
-      . (ExceptT . liftM Right)
-      . (ReaderT . const)
-      . \m -> StateT (\s -> do
-                         a <- m
-                         return (a, s))
-      ) (putStrLn "hello")
-    -- lift $ putStrLn "hello"
+    liftIO (putStrLn "hello")
     html $ mconcat ["<h1>Scotty, ", beam, " me up! </h1>"]
